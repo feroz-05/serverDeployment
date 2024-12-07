48c99f91812711fd77fabee0da816914a5d45ebc
feroz-005 <ferozcollegeuse@gmail.com>
2024-12-07 15:36:18 +0530

    Latest Comit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const user = require('./UserSchema');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.json());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 const port = 3000;
-
 
 mongoose.connect("mongodb+srv://ferozcollegeuse:feroz0125@ferozcluster01.h7aiv.mongodb.net/ferozdb").then(()=>{
     console.log("Mongodb Server Cloud is connected");
@@ -31,13 +31,34 @@ function CreateUser(name,email,password){
     }
 }
 
-
 app.get("/",(req,res)=>{
     res.send("Hello World");
-    const name = "Feroz";
-    const email = "feroz@gmail.com";
-    const password = "feroz";
-    CreateUser(name,email,password);
+})
+
+app.post("/register",async(req,res)=>{
+    const {name, email, password} = req.body;
+    const userExist = await user.findOne({email:email});
+    if(userExist){
+        res.send("User Already Exist");
+    }else{
+        CreateUser(name,email,password);
+        console.log(name,email,password);
+        console.log("User Created");
+        res.send("User Created Successfully");
+    }
+})
+
+app.post("/login",async(req,res)=>{
+    const {email, password} = req.body;
+    const userExist = await user.findOne({email:email});
+    if (userExist){
+        if(userExist.password === password){
+            res.send("Login Success");
+        }
+    }else{
+        res.send("user id and password does not match");
+    }
+
 })
 
 app.listen(port,()=>{
